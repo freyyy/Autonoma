0fa86d647d38bb18d07283d45ee2f13f75c37a46
cristian <rionutc94@gmail.com>
2018-12-07 23:32:03 +0200

    fix layout

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,104 +1,72 @@
 <template>
-    <v-app id="inspire">
-
-
-
-        <v-navigation-drawer
-                :clipped="$vuetify.breakpoint.lgAndUp"
-                v-model="drawer"
-                fixed
-                app
-        >
-            <v-list dense>
-                <template v-for="item in items">
-                    <v-layout
-                            v-if="item.heading"
-                            :key="item.heading"
-                            row
-                            align-center
-                    >
-                        <v-flex xs6>
-                            <v-subheader v-if="item.heading">
-                                {{ item.heading }}
-                            </v-subheader>
-                        </v-flex>
-                        <v-flex xs6 class="text-xs-center">
-                            <a href="#!" class="body-2 black--text">EDIT</a>
-                        </v-flex>
-                    </v-layout>
-                    <v-list-tile v-else :key="item.text" @click="$router.push(`/${item.href}`)">
-                        <v-list-tile-action>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>
-                                {{ item.text }}
-                            </v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-            </v-list>
-        </v-navigation-drawer>
-        <v-toolbar
-                :clipped-left="$vuetify.breakpoint.lgAndUp"
-                color="blue darken-3"
-                dark
-                app
-                fixed
-        >
-            <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-                <span class="hidden-sm-and-down">Autonoma</span>
-            </v-toolbar-title>
-            <v-text-field
-                    flat
-                    solo-inverted
-                    hide-details
-                    prepend-inner-icon="search"
-                    label="Search"
-                    class="hidden-sm-and-down"
-            ></v-text-field>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-                <v-icon>apps</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>notifications</v-icon>
-            </v-btn>
-        </v-toolbar>
-        <v-content>
-            <v-container fluid fill-height>
-                <v-layout justify-center align-center>
-
-                    <router-view>
-
-                    </router-view>
-
-
-
-                </v-layout>
-            </v-container>
-        </v-content>
-
-    </v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout v-if="item.heading" :key="item.heading" row align-center>
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-tile v-else :key="item.text" @click="$router.push(`/${item.href}`)">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-3" dark app fixed>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Autonoma</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            dialog: false,
-            drawer: null,
-            items: [
-                {icon: 'home', text: 'Dashboard', href: 'index'},
-                {icon: 'contacts', text: 'Team', href: 'team'},
-                {icon: 'content_copy', text: 'Departments', href: 'departments'},
-                {icon: 'settings', text: 'Settings'},
-                {icon: 'chat_bubble', text: 'Send feedback'},
-                {icon: 'help', text: 'Help'}
-            ]
-        }),
-        props: {
-            source: String
-        }
-    }
+export default {
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: "home", text: "Dashboard", href: "index" },
+      { icon: "contacts", text: "Team", href: "team" },
+      { icon: "content_copy", text: "Departments", href: "departments" },
+      { icon: "settings", text: "Settings" },
+      { icon: "chat_bubble", text: "Send feedback" },
+      { icon: "help", text: "Help" }
+    ]
+  }),
+  props: {
+    source: String
+  }
+};
 </script>
