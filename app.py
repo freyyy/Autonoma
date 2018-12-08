c48f38e2c0e9d204a352588ef5bb4ea96b151151
Stefan Andrei Balas <stefanandreibalas@Stefans-MacBook-Pro.local>
2018-12-08 07:28:34 +0200

    add cors config

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ app = Flask(__name__)
 CORS(app)
 model = Model()
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 @app.route('/employees', methods=['GET'])
 def employees():
     current_employees = read_users()
