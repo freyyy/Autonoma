7bf098ccd8357db0480758f287da1d89bc3c420d
Cristian RAITA <rionutc94@gmail.com>
2018-12-07 21:28:25 -0800

    unify deps tags

--- fe/src/components/Department.vue
+++ fe/src/components/Department.vue
@@ -1,20 +1,26 @@
 <script>
-    import { Doughnut } from 'vue-chartjs'
+import { Doughnut } from "vue-chartjs";
 
-    export default {
-        extends: Doughnut,
-        mounted () {
-            // Overwriting base render method with actual data.
-            this.renderChart({
-                labels: ["asdsadsads", "Asia", "Europe", "Latin America", "North America"],
-                datasets: [
-                    {
-                        label: "Population (millions)",
-                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                        data: [2478,5267,734,784,433]
-                    }
-                ]
-            })
+export default {
+  extends: Doughnut,
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ["HR", "IT", "Marketing", "Support", "Technical"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: [
+            "#3e95cd",
+            "#8e5ea2",
+            "#3cba9f",
+            "#e8c3b9",
+            "#c45850"
+          ],
+          data: [2478, 5267, 734, 784, 433]
         }
-    }
+      ]
+    });
+  }
+};
 </script>
